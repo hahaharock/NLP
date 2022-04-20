d7558d70f6b2f948aaf8930929810368cc009cf9
hahaharock <hahaharock@users.noreply.github.com>
2022-04-20 14:38:12 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.subheader("メッセージを入力してから送信をタップしてくだ
 message = st.text_input("メッセージ")
 
 
-def send_pya3rt(endpoint, apikey, text, callback):
+def send_pya3rt(endpoint, text, callback): # apikey, text, callback):
     params = {#'apikey': apikey,
               #'query': text,
               'key1': text,
@@ -30,14 +30,15 @@ def generate_response():
   
     #ans_json = send_pya3rt('https://api.a3rt.recruit.co.jp/talk/v1/smalltalk',
     #apikey, message, None)
-    ans_json = send_pya3rt('http://c5bb-35-233-238-146.ngrok.io/Check',
+    ans_json = send_pya3rt('http://51f9-35-233-238-146.ngrok.io/Check',
                        message, None)
-    ans = ans_json['results'][0]['reply']
-    chat_logs.append('you: ' + message)
-    chat_logs.append('AI: ' + ans)
-    for chat_log in chat_logs:
-        st.write(chat_log)
-
+    #ans = ans_json['results'][0]['reply']
+    #ans = ans_json['results'][0]['key1']
+    #chat_logs.append('you: ' + message)
+    #chat_logs.append('AI: ' + ans_json ) #ans)
+    #for chat_log in chat_logs:
+    #    st.write(chat_log)
+    st.write(ans_json)
 
 if st.button("送信"):
     generate_response()
