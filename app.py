074b6290c084bbd46f4156677e1bea1ef94369a4
hahaharock <hahaharock@users.noreply.github.com>
2022-04-20 14:03:43 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import requests
+
+apikey = "<APIキー>"
+
+chat_logs = []
+
+st.title("Chatbot with streamlit")
+
+st.subheader("メッセージを入力してから送信をタップしてください")
+
+message = st.text_input("メッセージ")
+
+
+def send_pya3rt(endpoint, apikey, text, callback):
+    params = {#'apikey': apikey,
+              #'query': text,
+              'key1': text,
+              }
+    if callback is not None:
+        params['callback'] = callback
+
+    response = requests.post(endpoint, params)
+    #response = requests.get(endpoint, params)
+
+    return response.json()
+
+
+def generate_response():
+  
+    #ans_json = send_pya3rt('https://api.a3rt.recruit.co.jp/talk/v1/smalltalk',
+    #apikey, message, None)
+    ans_json = send_pya3rt('http://c5bb-35-233-238-146.ngrok.io/Check',
+                       message, None)
+    ans = ans_json['results'][0]['reply']
+    chat_logs.append('you: ' + message)
+    chat_logs.append('AI: ' + ans)
+    for chat_log in chat_logs:
+        st.write(chat_log)
+
+
+if st.button("送信"):
+    generate_response()
