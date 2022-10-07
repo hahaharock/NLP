ecf4155cd35501fd5b0a2710b4443ff3c64c0f7a
hahaharock <hahaharock@users.noreply.github.com>
2022-10-07 13:35:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def send_pya3rt(endpoint, text, callback): # apikey, text, callback):
     if callback is not None:
         params['callback'] = callback
         
-    url = "https://proofreading.interlab-api.com:8000/Proof"
+    url = "https://www.interlab-api.com:8001/"
     headers = {"Content-Type": "application/json"}
     res = requests.post(url, headers=headers, data=json.dumps(data))
 
